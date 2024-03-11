b3862b6c86d7c3826bfefecb47a950b036d7414a
Anant Tripathi <anantraj.tripathi79@gmail.com>
2024-03-11 23:46:41 +0530

    setting up github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/gpt3-Anant/",
   plugins: [react()],
-  base: "gpt3-Anant",
 });
